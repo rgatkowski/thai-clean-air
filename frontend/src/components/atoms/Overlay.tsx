e64984fd03a35dfd9ffe93646199127ab244d3f7
maneroto <maneroto@hotmail.com>
2023-12-07 16:09:41 -0600

    Create environment section

--- frontend/src/components/atoms/Overlay.tsx
+++ frontend/src/components/atoms/Overlay.tsx
@@ -2,19 +2,21 @@ import { useRef, useEffect } from 'react';
 import styled from '@mui/material/styles/styled';
 
 import SmokeCanvas from '@tc/utils/SmokeCanvas';
+import clamp from '@tc/utils/clamp';
 
 interface OverlayProps {
   particlesNumber: number;
 }
 
-const StyledCanvas = styled('canvas')(() => ({
+const StyledCanvas = styled('canvas')((props) => ({
   width: '100vw',
   height: '100vh',
-  position: 'absolute',
+  position: 'fixed',
   top: 0,
   left: 0,
   userSelect: 'none',
   pointerEvents: 'none',
+  zIndex: 10,
 }));
 
 const Overlay = ({ particlesNumber }: OverlayProps) => {
@@ -38,7 +40,7 @@ const Overlay = ({ particlesNumber }: OverlayProps) => {
     animate(0);
   }, [particlesNumber]);
 
-  return <StyledCanvas ref={canvasRef} />;
+  return <StyledCanvas ref={canvasRef} sx={{  background: `rgba(0, 0, 0, ${clamp(0, particlesNumber, 60) / 100 - 0.1})` }} />;
 };
 
 export default Overlay;
