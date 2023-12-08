4d14d67829f03cbac89fd664dcb2e0fe8a6050e3
janwyrwinski <jan.wyrwinski@hogarth.com>
2023-12-08 04:34:45 +0000

    fixed retrieving pm25 value

--- frontend/src/server/APIrequests.ts
+++ frontend/src/server/APIrequests.ts
@@ -2,16 +2,25 @@ import { MeasuresAttributes, ArticlesAttributes } from '@tc/types/commonTypes';
 import server from './server';
 
 export const readMeasures = async (latitude: number, longitude: number) => {
-  const measures = await server.get<MeasuresAttributes>('/measures', {
+  const measures = await server.get<MeasuresAttributes, { data: { city: string, country: string, results: { value: string } }}>('/measures', {
     params: {
       location: `${latitude},${longitude}`,
       limit: 10,
     }
   });
 
-  return measures.data;
-};
+  const {
+    city,
+    country,
+    results: { value: pm25 },
+  } = measures.data;
 
+  return {
+    city,
+    country,
+    pm25,
+  };
+}
 export const readArticles = async (latitude: number, longitude: number) => {
   const articles = await server.get<ArticlesAttributes>(`/articles`, {
     params: {
